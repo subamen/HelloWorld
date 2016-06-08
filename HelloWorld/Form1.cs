980efe4650150fd174dad94009ac6166a741b94f
GitTestUser <GitTestUser>
2016-06-07 22:03:52 -0400

    added text box instead of message box

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -19,7 +19,7 @@ namespace HelloWorld
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello world.");
+            textBox1.Text = "Hello world.";
         }
 
         private void button2_Click(object sender, EventArgs e)
